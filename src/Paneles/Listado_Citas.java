52c8acd7254bd24f41ae01b556d6c876d6dc342a
Fernando Amador <Fernando Amador@192.168.0.13>
2023-10-21 22:48:48 -0600

    La funcionalidad de la Lista de Citas

--- src/Paneles/Listado_Citas.java
+++ src/Paneles/Listado_Citas.java
@@ -4,8 +4,6 @@ package Paneles;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
  */
-
-
 import static App.Menu.panelprincipal;
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -40,16 +38,16 @@ public class Listado_Citas extends javax.swing.JPanel {
         initComponents();
         cargarTablaEmpleados();
 
-        holder = new TextPrompt("Busque por nombre/apellido del empleado", txtBuscar);
+        holder = new TextPrompt("Busque por nombre/apellido del cliente", txtBuscar);
 
-        tblEmpleados.getTableHeader().setFont(new Font("Segoe UI", Font.BOLD, 12));
-        tblEmpleados.getTableHeader().setOpaque(false);
-        tblEmpleados.getTableHeader().setBackground(new Color(255, 0, 0));
-        tblEmpleados.getTableHeader().setForeground(new Color(255, 0, 0));
-        tblEmpleados.setRowHeight(25);
+        tblCitas.getTableHeader().setFont(new Font("Segoe UI", Font.BOLD, 12));
+        tblCitas.getTableHeader().setOpaque(false);
+        tblCitas.getTableHeader().setBackground(new Color(255, 0, 0));
+        tblCitas.getTableHeader().setForeground(new Color(255, 0, 0));
+        tblCitas.setRowHeight(25);
 
-        tblEmpleados.setRowSelectionAllowed(true);
-        tblEmpleados.setColumnSelectionAllowed(false);
+        tblCitas.setRowSelectionAllowed(true);
+        tblCitas.setColumnSelectionAllowed(false);
     }
 
     /**
@@ -70,7 +68,7 @@ public class Listado_Citas extends javax.swing.JPanel {
         verbtn = new javax.swing.JButton();
         Texto_Buscar = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
-        tblEmpleados = new javax.swing.JTable();
+        tblCitas = new javax.swing.JTable();
         txtBuscar = new javax.swing.JTextField();
         Btn_Buscar = new javax.swing.JButton();
         refrescarbtn = new javax.swing.JButton();
@@ -159,27 +157,27 @@ public class Listado_Citas extends javax.swing.JPanel {
         Texto_Buscar.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         Texto_Buscar.setText("Buscar:");
 
-        tblEmpleados.setModel(new javax.swing.table.DefaultTableModel(
+        tblCitas.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
             new String [] {
-                "N°", "Nombre Cliente", "Celular", "Fecha de la cita"
+                "N°", "Nombre Cliente", "Apellido Cliente", "Celular", "Fecha de la cita"
             }
         ) {
             boolean[] canEdit = new boolean [] {
-                false, false, false, false
+                false, false, false, false, false
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 return canEdit [columnIndex];
             }
         });
-        tblEmpleados.setGridColor(new java.awt.Color(255, 51, 51));
-        tblEmpleados.setSelectionBackground(new java.awt.Color(255, 102, 102));
-        tblEmpleados.setShowHorizontalLines(true);
-        tblEmpleados.setShowVerticalLines(true);
-        jScrollPane2.setViewportView(tblEmpleados);
+        tblCitas.setGridColor(new java.awt.Color(255, 51, 51));
+        tblCitas.setSelectionBackground(new java.awt.Color(255, 102, 102));
+        tblCitas.setShowHorizontalLines(true);
+        tblCitas.setShowVerticalLines(true);
+        jScrollPane2.setViewportView(tblCitas);
 
         txtBuscar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -304,8 +302,7 @@ public class Listado_Citas extends javax.swing.JPanel {
     private void txtBuscarKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtBuscarKeyTyped
         // validación en el campo de buscar para que acepte solo letra y letras tildades, se utiliza el codigo ASCII para las letras tildades
         char c = evt.getKeyChar();
-        if ((c < 'a' || c > 'z') && (c < 'A' || c > 'Z') && ((int) evt.getKeyChar() <= 160) || txtBuscar.getText().length() >= 20)
-        {
+        if ((c < 'a' || c > 'z') && (c < 'A' || c > 'Z') && ((int) evt.getKeyChar() <= 160) || txtBuscar.getText().length() >= 20) {
             evt.consume();
         }
     }//GEN-LAST:event_txtBuscarKeyTyped
@@ -314,12 +311,10 @@ public class Listado_Citas extends javax.swing.JPanel {
         String texto = txtBuscar.getText().trim();
 
         //Validacion del texto ingresado
-        if (!texto.isEmpty())
-        {
+        if (!texto.isEmpty()) {
             buscarDatos(texto);
-        } else
-        {
-            JOptionPane.showMessageDialog(null, "El texto ingresado es erroneo");
+        } else {
+            JOptionPane.showMessageDialog(null, "Tiene que ingrese texto");
         }
     }//GEN-LAST:event_Btn_BuscarActionPerformed
 
@@ -329,6 +324,7 @@ public class Listado_Citas extends javax.swing.JPanel {
 
         // Limpiar el campo de búsqueda
         txtBuscar.setText("");
+        holder = new TextPrompt("Busque por nombre/apellido del cliente", txtBuscar);
 
         // Cargar la tabla con los datos actualizados
         cargarTablaEmpleados();
@@ -355,13 +351,15 @@ public class Listado_Citas extends javax.swing.JPanel {
         //
     }//GEN-LAST:event_editarbtnActionPerformed
 
-    int filasPorPagina = 20; // Cantidad de filas que se mostrarán en cada página
     int paginaActual = 1; // Página actual
-    int totalPaginas = 1; // Total de páginas
+    int filasPorPagina = 15; // Número de filas a mostrar por página
+    int totalFilas = 0; // Total de filas en la tabla
+    int totalPaginas = 0; // Total de páginas en la tabla
+    int numRegistro = 0;
     String terminoBusqueda = ""; // Término de búsqueda actual
 
     private void cargarTablaEmpleados() {
-        /*DefaultTableModel modeloTabla = (DefaultTableModel) tblEmpleados.getModel();
+        DefaultTableModel modeloTabla = (DefaultTableModel) tblCitas.getModel();
         modeloTabla.setRowCount(0); // Limpiar los datos existentes en la tabla
 
         PreparedStatement ps;
@@ -370,38 +368,41 @@ public class Listado_Citas extends javax.swing.JPanel {
         int columnas;
         boolean foundData = false;
 
-        try
-        {
+        try {
             Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=GlendaDB;encrypt=true;trustServerCertificate=true;", "sa", "123456789");
 
-            ps = conn.prepareStatement("SELECT COUNT(*) AS TotalFilas FROM Empleado WHERE nombre1 LIKE ? OR apellido1 LIKE ?");
+            // Obtener el total de filas que cumplen con el criterio de búsqueda
+            ps = conn.prepareStatement("SELECT COUNT(*) AS TotalFilas "
+                    + "FROM Cliente E "
+                    + "JOIN Cita V ON E.id_cliente = V.id_cliente "
+                    + "WHERE E.nombre LIKE ? OR E.apellido LIKE ?");
             ps.setString(1, "%" + terminoBusqueda + "%");
             ps.setString(2, "%" + terminoBusqueda + "%");
             rs = ps.executeQuery();
 
-            int cantidadFilas = 0;
-            if (rs.next())
-            {
-                cantidadFilas = rs.getInt("TotalFilas");
+            if (rs.next()) {
+                totalFilas = rs.getInt(1);
             }
+            totalPaginas = (int) Math.ceil((double) totalFilas / filasPorPagina);
 
-            totalPaginas = (int) Math.ceil((double) cantidadFilas / filasPorPagina);
-
-            if (paginaActual < 1)
-            {
+            if (paginaActual < 1) {
                 paginaActual = 1;
-            } else if (paginaActual > totalPaginas)
-            {
+            } else if (paginaActual > totalPaginas) {
                 paginaActual = totalPaginas;
             }
 
             int offset = (paginaActual - 1) * filasPorPagina;
-            if (offset < 0)
-            {
+            if (offset < 0) {
                 offset = 0;
             }
 
-            ps = conn.prepareStatement("SELECT ROW_NUMBER() OVER(ORDER BY nombre1) AS NumRegistro, nombre1, apellido1, celular, barrio FROM Empleado WHERE nombre1 LIKE ? OR apellido1 LIKE ? ORDER BY nombre1 OFFSET ? ROWS FETCH NEXT ? ROWS ONLY");
+            // Consulta para obtener los datos paginados
+            ps = conn.prepareStatement("SELECT ROW_NUMBER() OVER(ORDER BY E.nombre) AS NumRegistro, E.nombre, E.apellido, E.numero_telefono, V.fecha_cita "
+                    + "FROM Cliente E "
+                    + "JOIN Cita V ON E.id_cliente = V.id_cliente "
+                    + "WHERE E.nombre LIKE ? OR E.apellido LIKE ? "
+                    + "ORDER BY E.nombre "
+                    + "OFFSET ? ROWS FETCH NEXT ? ROWS ONLY");
             ps.setString(1, "%" + terminoBusqueda + "%");
             ps.setString(2, "%" + terminoBusqueda + "%");
             ps.setInt(3, offset);
@@ -410,92 +411,96 @@ public class Listado_Citas extends javax.swing.JPanel {
             rsmd = rs.getMetaData();
             columnas = rsmd.getColumnCount();
 
-            while (rs.next())
-            {
+            while (rs.next()) {
                 Object[] fila = new Object[columnas];
-                for (int indice = 0; indice < columnas; indice++)
-                {
+                for (int indice = 0; indice < columnas; indice++) {
                     fila[indice] = rs.getObject(indice + 1);
                 }
                 modeloTabla.addRow(fila);
                 foundData = true;
             }
 
-            if (!foundData)
-            {
+            ajustarTabla(filasPorPagina);
+
+            if (!foundData) {
                 JOptionPane.showMessageDialog(null, "No se encontraron datos");
             }
 
-            Texto_Contable.setText("Cantidad de filas: " + cantidadFilas + " - Página " + paginaActual + " de " + totalPaginas);
+            // Obtener el número de filas actualizado
+            int rowCount = modeloTabla.getRowCount();
+            Texto_Contable.setText("Cantidad de filas: " + rowCount + " - Página " + paginaActual + "/" + totalPaginas);
 
-        } catch (SQLException e)
-        {
+        } catch (SQLException e) {
+            e.printStackTrace(); // Imprime la pila de excepciones para depuración
             JOptionPane.showMessageDialog(null, e.toString());
-        }*/
+        }
+
+    }
+
+    private void ajustarTabla(int filasDeseadas) {
+        tblCitas.setPreferredScrollableViewportSize(new Dimension(tblCitas.getPreferredSize().width, tblCitas.getRowHeight() * filasDeseadas));
+        tblCitas.setFillsViewportHeight(true);
     }
 
     private void siguientePagina() {
-        if (paginaActual < totalPaginas)
-        {
+        if (paginaActual < totalPaginas) {
             paginaActual++;
             cargarTablaEmpleados();
         }
     }
 
     private void paginaAnterior() {
-        if (paginaActual > 1)
-        {
+        if (paginaActual > 1) {
             paginaActual--;
             cargarTablaEmpleados();
         }
     }
 
-//    private void ajustarTabla(int filasDeseadas) {
-//        tblEmpleados.setPreferredScrollableViewportSize(new Dimension(tblEmpleados.getPreferredSize().width, tblEmpleados.getRowHeight() * filasDeseadas));
-//        tblEmpleados.setFillsViewportHeight(true);
-//    }
-
-      private void buscarDatos(String texto) {
-        DefaultTableModel modelTabla = (DefaultTableModel) tblEmpleados.getModel();
+    private void buscarDatos(String texto) {
+        DefaultTableModel modelTabla = (DefaultTableModel) tblCitas.getModel();
         modelTabla.setRowCount(0);
         boolean foundData = false;
 
-        try
-        {
+        try {
             Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=GlendaDB;encrypt=true;trustServerCertificate=true;", "sa", "123456789");
-            if (conn != null && !conn.isClosed())
-            {
-                PreparedStatement ps = conn.prepareStatement("SELECT ROW_NUMBER() OVER(ORDER BY nombre1) AS NumRegistro, nombre1, apellido1, celular, barrio FROM Empleado WHERE nombre1 LIKE ? OR apellido1 LIKE ?");
+            if (conn != null && !conn.isClosed()) {
+                PreparedStatement ps = conn.prepareStatement("SELECT ROW_NUMBER() OVER(ORDER BY E.nombre) AS NumRegistro, E.nombre, E.apellido, E.numero_telefono, V.fecha_cita "
+                        + "FROM Cliente E "
+                        + "JOIN Cita V ON E.id_cliente = V.id_cliente "
+                        + "WHERE E.nombre LIKE ? OR E.apellido LIKE ? "
+                        + "ORDER BY E.nombre "
+                        + "OFFSET ? ROWS FETCH NEXT ? ROWS ONLY");
 
-                if (texto != null && !texto.isEmpty())
-                {
+                if (texto != null && !texto.isEmpty()) {
                     ps.setString(1, "%" + texto + "%");
                     ps.setString(2, "%" + texto + "%");
                     terminoBusqueda = texto; // Actualizar el término de búsqueda
-                } else
-                {
+                } else {
                     ps.setString(1, "%");
                     ps.setString(2, "%");
                     terminoBusqueda = ""; // Limpiar el término de búsqueda
                 }
 
+                // Define el OFFSET y FETCH NEXT de acuerdo a tus necesidades
+                int offset = 0; // Cambia el valor del offset según tus requerimientos
+                int fetchNext = 10; // Cambia la cantidad de registros a recuperar según tus requerimientos
+
+                ps.setInt(3, offset);
+                ps.setInt(4, fetchNext);
+
                 ResultSet rs = ps.executeQuery();
 
-                if (rs != null)
-                {
-                    while (rs.next())
-                    {
+                if (rs != null) {
+                    while (rs.next()) {
                         int numRegistro = rs.getInt("NumRegistro");
-                        String nombre = rs.getString("nombre1");
-                        String Dni = rs.getString("apellido1");
-                        String departamento = rs.getString("celular");
-                        String deducciones = rs.getString("barrio");
+                        String nombre = rs.getString("nombre");
+                        String apellido = rs.getString("apellido");
+                        String numeroTelefono = rs.getString("numero_telefono");
+                        String fechaCita = rs.getString("fecha_cita");
 
-                        if (nombre != null && Dni != null && departamento != null)
-                        {
-                            modelTabla.addRow(new Object[]
-                            {
-                                numRegistro, nombre, Dni, departamento, deducciones
+                        if (nombre != null && apellido != null && numeroTelefono != null) {
+                            modelTabla.addRow(new Object[]{
+                                numRegistro, nombre, apellido, numeroTelefono, fechaCita
                             });
                             foundData = true;
                         }
@@ -507,11 +512,11 @@ public class Listado_Citas extends javax.swing.JPanel {
                 ps.close();
                 conn.close();
             }
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e.toString());
         }
 
+        // Llama a la función de cargarTablaEmpleados() si es necesario recargar la tabla después de la búsqueda
         cargarTablaEmpleados(); // Recargar la tabla después de la búsqueda
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -528,7 +533,7 @@ public class Listado_Citas extends javax.swing.JPanel {
     private javax.swing.JPanel jPanel4;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JButton refrescarbtn;
-    private javax.swing.JTable tblEmpleados;
+    private javax.swing.JTable tblCitas;
     public javax.swing.JTextField txtBuscar;
     private javax.swing.JButton verbtn;
     // End of variables declaration//GEN-END:variables
