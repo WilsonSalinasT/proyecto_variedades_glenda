45eb5fa15b8655bd391cb768ba64cbbe7884e9ad
Fernando Amador <Fernando Amador@192.168.0.13>
2023-11-05 22:12:07 -0600

    Ya crea en el apartado de sublimacion

--- src/Paneles/Crear_Pedido_Sublimacion.java
+++ src/Paneles/Crear_Pedido_Sublimacion.java
@@ -6,19 +6,27 @@ package Paneles;
 
 import static App.Menu.panelprincipal;
 import java.awt.BorderLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  *
@@ -160,8 +168,8 @@ public class Crear_Pedido_Sublimacion extends javax.swing.JPanel {
         jLabel5 = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
         btnCrear = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
+        imagen = new javax.swing.JLabel();
+        AgregarImagen = new javax.swing.JButton();
         jDateChooser1 = new com.toedter.calendar.JDateChooser();
         cbxEstado = new javax.swing.JComboBox<>();
 
@@ -263,11 +271,16 @@ public class Crear_Pedido_Sublimacion extends javax.swing.JPanel {
             }
         });
 
-        jLabel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        imagen.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
-        jButton2.setBackground(new java.awt.Color(255, 153, 51));
-        jButton2.setFont(new java.awt.Font("Arial", 3, 12)); // NOI18N
-        jButton2.setText("Agregar imagen");
+        AgregarImagen.setBackground(new java.awt.Color(255, 153, 51));
+        AgregarImagen.setFont(new java.awt.Font("Arial", 3, 12)); // NOI18N
+        AgregarImagen.setText("Agregar imagen");
+        AgregarImagen.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AgregarImagenActionPerformed(evt);
+            }
+        });
 
         jDateChooser1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Fecha de entrega:", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.TOP, new java.awt.Font("Arial Black", 3, 12))); // NOI18N
 
@@ -322,10 +335,10 @@ public class Crear_Pedido_Sublimacion extends javax.swing.JPanel {
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGap(130, 130, 130)
-                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 276, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(imagen, javax.swing.GroupLayout.PREFERRED_SIZE, 276, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGap(217, 217, 217)
-                                .addComponent(jButton2))
+                                .addComponent(AgregarImagen))
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGap(101, 101, 101)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
@@ -371,9 +384,9 @@ public class Crear_Pedido_Sublimacion extends javax.swing.JPanel {
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addComponent(txtPrecio, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(29, 29, 29)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 294, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(imagen, javax.swing.GroupLayout.PREFERRED_SIZE, 294, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(35, 35, 35)
-                        .addComponent(jButton2)
+                        .addComponent(AgregarImagen)
                         .addGap(60, 60, 60)
                         .addComponent(btnCrear, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(30, 30, 30))))
@@ -393,9 +406,7 @@ public class Crear_Pedido_Sublimacion extends javax.swing.JPanel {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 526, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 728, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -449,87 +460,102 @@ public class Crear_Pedido_Sublimacion extends javax.swing.JPanel {
     }
 
     private void btnCrearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCrearActionPerformed
-        /*String nombre = txtnombre.getText().trim();
+        String nombre = (String) txtCliente.getSelectedItem();
+        String material = (String) cbnproducto.getSelectedItem();
         String descripcion = txtdescripcion.getText().trim();
-        String categoria = (String) jComboBox1.getSelectedItem();
+        String estado = (String) cbxEstado.getSelectedItem();
+        String precio = txtPrecio.getText().trim();
+        String cantidad = txtcantidad.getText().trim();
+        SimpleDateFormat sdfSQL = new SimpleDateFormat("yyyy-MM-dd");
+        String fechaPedido = sdfSQL.format(new Date());
+        String imagenM1 = imagen.getText();
+        String idC = id_cliente.getText();
 
         StringBuilder camposVacios = new StringBuilder("Los siguientes campos están vacíos:");
 
-        if (nombre.isEmpty())
-        {
-            camposVacios.append("\n - Nombre");
+        if (nombre.equals("Seleccione")) {
+            camposVacios.append("\n - Nombre del cliente");
         }
-        if (descripcion.isEmpty())
-        {
+        
+        if (descripcion.isEmpty()) {
             camposVacios.append("\n - Descripcion");
         }
-        String categorias = jComboBox1.getSelectedItem().toString(); // Obtiene el elemento seleccionado en el JComboBox
+        String materiales = cbnproducto.getSelectedItem().toString(); // Obtiene el elemento seleccionado en el JComboBox
 
-        if (categorias.equals("Seleccione"))
-        {
-            camposVacios.append("\n - Categoria");
+        if (materiales.equals("Seleccione")) {
+            camposVacios.append("\n - Material");
         }
-        if (!camposVacios.toString().equals("Los siguientes campos están vacíos:"))
-        {
-            JOptionPane.showMessageDialog(null, camposVacios.toString(), "Campos Vacíos", JOptionPane.ERROR_MESSAGE);
-        } else
-        {
 
-            try
-            {
-                // Resto del código para la inserción en la base de datos
+        if (cantidad.isEmpty()) {
+            camposVacios.append("\n - Cantidad");
+        }
+
+        if (imagen.getIcon() == null) {
+            camposVacios.append("\n - Agregue una imagen de muestra");
+        }
+
+        if (fechaPedido == null) {
+            camposVacios.append("\n - Fecha de Entrega");
+        }
+
+        if (!camposVacios.toString().equals("Los siguientes campos están vacíos:")) {
+            JOptionPane.showMessageDialog(null, camposVacios.toString(), "Campos Vacíos", JOptionPane.ERROR_MESSAGE);
+        } else {
+
+            try {
+                // Rutas de las imágenes
+                String rutaImagen1 = archivos[0].getAbsolutePath();
+                System.out.println("archivos[0]: " + archivos[0]);
+
                 Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
                 Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=GlendaDB;encrypt=true;trustServerCertificate=true;", "sa", "123456789");
 
-                // Verifica si ya existe un producto con el mismo nombre
-                PreparedStatement checkNamePs = conn.prepareStatement("SELECT COUNT(*) FROM Producto WHERE nombre = ?");
-                checkNamePs.setString(1, nombre);
-                ResultSet result = checkNamePs.executeQuery();
-                result.next();
-                int count = result.getInt(1);
+                PreparedStatement insertPs = conn.prepareStatement("INSERT INTO PedidoSublimacion (material, estado, descripcion, precio,"
+                        + "imagen1,fechaPedido,id_cliente) "
+                        + "VALUES (?,?,?,?,?,?,?)");
+                insertPs.setString(1, material);
+                insertPs.setString(2, estado);
+                insertPs.setObject(3, descripcion);
+                insertPs.setObject(4, precio);
 
-                if (count > 0)
-                {
-                    // Obtenemos el nombre del producto que ya existe
-                    PreparedStatement getExistingNamePs = conn.prepareStatement("SELECT nombre FROM Producto WHERE nombre = ?");
-                    getExistingNamePs.setString(1, nombre);
-                    ResultSet existingNameResult = getExistingNamePs.executeQuery();
-                    existingNameResult.next();
-                    String existingName = existingNameResult.getString("nombre");
-
-                    JOptionPane.showMessageDialog(null, "El producto con nombre '" + existingName + "' ya existe. Por favor, elige otro nombre.");
-                } else
-                {
-                    // Si el nombre es único, procede con la inserción
-                    PreparedStatement insertPs = conn.prepareStatement("INSERT INTO Producto (nombre,descripcion,categoria) VALUES (?,?,?)");
-                    insertPs.setString(1, nombre);
-                    insertPs.setString(2, descripcion);
-                    insertPs.setString(3, categoria);
-
-                    insertPs.executeUpdate();
-                    JOptionPane.showMessageDialog(null, "Registro guardado");
-
-                    Listado_Productos cli = new Listado_Productos();
-
-                    cli.setSize(1024, 640);
-                    cli.setLocation(0, 0);
-
-                    panelprincipal.revalidate();
-                    panelprincipal.repaint();
-                    panelprincipal.removeAll();
-                    panelprincipal.add(cli, BorderLayout.CENTER);
-                    panelprincipal.revalidate();
-                    panelprincipal.repaint();
+                // Verificar si el archivo de imagen 1 existe y agregarlo si es el caso
+                if (rutaImagen1 != null && !rutaImagen1.isEmpty() && new File(rutaImagen1).exists()) {
+                    try {
+                        FileInputStream fis1 = new FileInputStream(new File(rutaImagen1));
+                        insertPs.setBinaryStream(5, fis1, (int) new File(rutaImagen1).length());
+                    } catch (FileNotFoundException e) {
+                        // Manejar la excepción si el archivo no se encuentra
+                        insertPs.setBinaryStream(5, null, 0); // Imagen 1 no existe
+                    }
+                } else {
+                    insertPs.setBinaryStream(5, null, 0); // Imagen 1 no existe
                 }
 
-            } catch (SQLException e)
-            {
+                insertPs.setString(6, fechaPedido);
+                // insertPs.setDate(24, new java.sql.Date(fecha.getTime()));
+                insertPs.setObject(7, idC);
+
+                insertPs.executeUpdate();
+                JOptionPane.showMessageDialog(null, "Registro guardado");
+
+                Listado_Sublimacion sub = new Listado_Sublimacion();
+
+                sub.setSize(1024, 640);
+                sub.setLocation(0, 0);
+
+                panelprincipal.revalidate();
+                panelprincipal.repaint();
+                panelprincipal.removeAll();
+                panelprincipal.add(sub, BorderLayout.CENTER);
+                panelprincipal.revalidate();
+                panelprincipal.repaint();
+
+            } catch (SQLException e) {
                 JOptionPane.showMessageDialog(null, e.toString(), "Error de SQL", JOptionPane.ERROR_MESSAGE);
-            } catch (ClassNotFoundException ex)
-            {
+            } catch (ClassNotFoundException ex) {
                 JOptionPane.showMessageDialog(null, "Error de conexión a la base de datos", "Error de Conexión", JOptionPane.ERROR_MESSAGE);
             }
-        }*/
+        }
     }//GEN-LAST:event_btnCrearActionPerformed
 
     private void txtcantidadActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtcantidadActionPerformed
@@ -553,16 +579,42 @@ public class Crear_Pedido_Sublimacion extends javax.swing.JPanel {
 
     }//GEN-LAST:event_txtcantidadKeyTyped
 
+    private File[] archivos = new File[1];
+    private int contador = 0; // Para llevar el registro de cuántas veces se ha presionado el botón
+
+    private void AgregarImagenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AgregarImagenActionPerformed
+        if (contador < 3) {
+            JFileChooser fclAbrirArchivo = new JFileChooser();
+            fclAbrirArchivo.setFileFilter(new FileNameExtensionFilter("Archivo de imagen", "jpg", "jpeg", "png"));
+
+            int respuesta = fclAbrirArchivo.showOpenDialog(this);
+
+            if (respuesta == JFileChooser.APPROVE_OPTION) {
+                archivos[contador] = fclAbrirArchivo.getSelectedFile();
+
+                // Cargar la imagen en el label correspondiente (imagen1, imagen2, o imagen3)
+                ImageIcon icono = new ImageIcon(archivos[contador].getAbsolutePath());
+                Image foto = icono.getImage().getScaledInstance(imagen.getWidth(), imagen.getHeight(), Image.SCALE_DEFAULT);
+
+                if (contador == 0) {
+                    imagen.setIcon(new ImageIcon(foto));
+                }
+
+                contador++;
+            }
+        }
+    }//GEN-LAST:event_AgregarImagenActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton AgregarImagen;
     private javax.swing.JButton btnCrear;
     private javax.swing.JButton btnVolve;
     private static final javax.swing.JComboBox<String> cbnproducto = new javax.swing.JComboBox<>();
     private javax.swing.JComboBox<String> cbxEstado;
     public static final javax.swing.JLabel id_cliente = new javax.swing.JLabel();
-    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel imagen;
     private com.toedter.calendar.JDateChooser jDateChooser1;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel5;
