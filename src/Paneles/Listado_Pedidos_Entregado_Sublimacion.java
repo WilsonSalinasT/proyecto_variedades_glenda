845e4762a55e83a210080b61e08099ec1ad90515
novastar <novastar@DESKTOP-UI6IBT9>
2023-11-08 21:26:54 -0600

    pequeños cambios

--- src/Paneles/Listado_Pedidos_Entregado_Sublimacion.java
+++ src/Paneles/Listado_Pedidos_Entregado_Sublimacion.java
@@ -376,7 +376,7 @@ public class Listado_Pedidos_Entregado_Sublimacion extends javax.swing.JPanel {
             ps = conn.prepareStatement("SELECT COUNT(*) AS TotalFilas "
                     + "FROM Cliente E "
                     + "JOIN PedidoSublimacion V ON E.id_cliente = V.id_cliente "
-                    + "WHERE E.nombre LIKE ? OR E.apellido LIKE ? OR V.material LIKE ?");
+                    + "WHERE (E.nombre LIKE ? OR E.apellido LIKE ? OR V.material LIKE ?) and estado = 'entregado' ");
             ps.setString(1, "%" + terminoBusqueda + "%");
             ps.setString(2, "%" + terminoBusqueda + "%");
             ps.setString(3, "%" + terminoBusqueda + "%");
