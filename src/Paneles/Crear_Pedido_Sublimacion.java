f5946af7cff5761ac9bef01458df4532a005f87e
Fernando Amador <Fernando Amador@192.168.0.13>
2023-11-18 22:41:21 -0600

    Eliminar boton de agregar imagen - sublimacion

--- src/Paneles/Crear_Pedido_Sublimacion.java
+++ src/Paneles/Crear_Pedido_Sublimacion.java
@@ -170,7 +170,6 @@ public class Crear_Pedido_Sublimacion extends javax.swing.JPanel {
         jDateChooser1 = new com.toedter.calendar.JDateChooser();
         cbxEstado = new javax.swing.JComboBox<>();
         jPanel2 = new javax.swing.JPanel();
-        AgregarImagen = new javax.swing.JButton();
         imagen = new javax.swing.JLabel();
 
         jLabel10.setFont(new java.awt.Font("Segoe Print", 1, 14)); // NOI18N
@@ -298,39 +297,32 @@ public class Crear_Pedido_Sublimacion extends javax.swing.JPanel {
         jPanel2.setBackground(new java.awt.Color(255, 255, 255));
         jPanel2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 0, 0)));
 
-        AgregarImagen.setBackground(new java.awt.Color(255, 153, 51));
-        AgregarImagen.setFont(new java.awt.Font("Arial", 3, 12)); // NOI18N
-        AgregarImagen.setText("Agregar imagen");
-        AgregarImagen.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                AgregarImagenActionPerformed(evt);
+        imagen.setFont(new java.awt.Font("Arial Black", 2, 18)); // NOI18N
+        imagen.setForeground(new java.awt.Color(153, 153, 153));
+        imagen.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        imagen.setText("Agrega una imagen");
+        imagen.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 0, 0)));
+        imagen.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                imagenMouseClicked(evt);
             }
         });
-
-        imagen.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 0, 0)));
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addContainerGap(23, Short.MAX_VALUE)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                        .addComponent(AgregarImagen)
-                        .addGap(100, 100, 100))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                        .addComponent(imagen, javax.swing.GroupLayout.PREFERRED_SIZE, 284, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(20, 20, 20))))
+                .addGap(14, 14, 14)
+                .addComponent(imagen, javax.swing.GroupLayout.PREFERRED_SIZE, 299, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(14, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addGap(18, 18, 18)
-                .addComponent(AgregarImagen)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(imagen, javax.swing.GroupLayout.PREFERRED_SIZE, 270, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(29, Short.MAX_VALUE))
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(30, 30, 30)
+                .addComponent(imagen, javax.swing.GroupLayout.PREFERRED_SIZE, 279, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(41, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -506,7 +498,6 @@ public class Crear_Pedido_Sublimacion extends javax.swing.JPanel {
             camposVacios.append("\n - Agregue una imagen de muestra");
         }
 
-
         if (fechaPedido == null) {
             camposVacios.append("\n - Fecha de Entrega");
         }
@@ -596,8 +587,9 @@ public class Crear_Pedido_Sublimacion extends javax.swing.JPanel {
     private File[] archivos = new File[1];
     private int contador = 0; // Para llevar el registro de cuántas veces se ha presionado el botón
 
-    private void AgregarImagenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AgregarImagenActionPerformed
-        if (contador < 3) {
+    private void imagenMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_imagenMouseClicked
+
+        if (contador == 0) {
             JFileChooser fclAbrirArchivo = new JFileChooser();
             fclAbrirArchivo.setFileFilter(new FileNameExtensionFilter("Archivo de imagen", "jpg", "jpeg", "png"));
 
@@ -610,19 +602,19 @@ public class Crear_Pedido_Sublimacion extends javax.swing.JPanel {
                 ImageIcon icono = new ImageIcon(archivos[contador].getAbsolutePath());
                 Image foto = icono.getImage().getScaledInstance(imagen.getWidth(), imagen.getHeight(), Image.SCALE_DEFAULT);
 
-                if (contador == 0) {
-                    imagen.setIcon(new ImageIcon(foto));
-                    AgregarImagen.setEnabled(false); // Desactivar el botón después de cargar la primera imagen
-                }
+                imagen.setIcon(new ImageIcon(foto));
+                //imagen.setEnabled(false); // Desactivar el botón después de cargar la primera imagen
 
                 contador++;
             }
+        } else {
+            JOptionPane.showMessageDialog(this, "Ya se ha cargado una imagen. No se puede agregar otra.", "Advertencia", JOptionPane.WARNING_MESSAGE);
         }
-    }//GEN-LAST:event_AgregarImagenActionPerformed
+
+    }//GEN-LAST:event_imagenMouseClicked
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton AgregarImagen;
     private javax.swing.JButton btnCrear;
     private javax.swing.JButton btnVolve;
     private static final javax.swing.JComboBox<String> cbnproducto = new javax.swing.JComboBox<>();
